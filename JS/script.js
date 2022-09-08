a6ba47effdab7fa48e631ef134bb02ded2e83309
aru55barri <fran5barri@gmail.com>
2022-09-08 17:30:59 +0200

    primera subida del encriptador de texto

---
+++ JS/script.js
@@ -0,0 +1,73 @@
+const texto_aqui = document.querySelector(".texto_aqui")
+const mensaje_E_D = document.querySelector(".mensaje_E_D")
+const muneco = document.querySelector(".muneco");
+const encript_result = document.querySelector(".encript-result");
+
+encript_result.style.display="none";
+
+function btn_encriptar(){
+    const textoEncriptado = encriptar(texto_aqui.value);
+    mensaje_E_D.value = textoEncriptado;
+
+    if (texto_aqui.value == ""){
+        encript_result.style.display = "none";
+        muneco.style.display = "";
+    } else {
+        encript_result.style.display = "";
+        muneco.style.display = "none";
+        texto_aqui.value = "";
+    }
+}
+
+function encriptar(stringEncript){
+    let letrasEncript = [["e", "enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+    stringEncript = stringEncript.toLowerCase();
+
+    for (let i = 0; i < letrasEncript.length;  i++){
+        if(stringEncript.includes(letrasEncript[i][0])){
+            stringEncript=stringEncript.replaceAll
+            (letrasEncript[i][0],letrasEncript[i][1]);
+        }
+    }
+
+
+    return stringEncript;
+}
+
+function btn_desencriptar(){
+    const textoDesencriptado = desencriptar(texto_aqui.value);
+    mensaje_E_D.value = textoDesencriptado;
+
+    if (texto_aqui.value == ""){
+        encript_result.style.display = "none";
+        muneco.style.display = "";
+    } else{
+        encript_result.style.display = "";
+        muneco.style.display = "none";
+        texto_aqui.value = "";
+    }
+}
+
+function desencriptar(stringDesencript){
+    let matrizCodigo = [["e", "enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+    stringDesencript = stringDesencript.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length;  i++){
+        if(stringDesencript.includes(matrizCodigo[i][1])){
+            stringDesencript=stringDesencript.replaceAll
+            (matrizCodigo[i][1],matrizCodigo[i][0]);
+        }
+    }
+
+
+    return stringDesencript;
+}
+
+function btn_copiar(){
+    mensaje_E_D.select();
+    navigator.clipboard.writeText(mensaje_E_D.value);
+    mensaje_E_D.value = "";
+    alert("Mensaje copiado exitosamente");
+}
